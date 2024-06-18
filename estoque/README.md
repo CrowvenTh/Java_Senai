06eca538eb2f3bf14086df3568031af6a0fe502f
CrowvenTh <129080167+CrowvenTh@users.noreply.github.com>
2024-06-18 17:12:14 -0300

    estoque update

--- estoque/README.md
+++ estoque/README.md
@@ -16,3 +16,6 @@ Meanwhile, the compiled output files will be generated in the `bin` folder by de
 ## Dependency Management
 
 The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+
+18/06/2024
+## Verificar se o produto está dentro do prazo de validade & quantos dias falatam para o produto vencer.
--- estoque/src/controle/Pao.java
+++ estoque/src/controle/Pao.java
@@ -1,7 +1,6 @@
 package controle;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Pao extends Produto{
     private String tipoPao;
@@ -12,14 +11,43 @@ public class Pao extends Produto{
     public String setTipoPao(String tipoPao){
         return this.tipoPao = tipoPao;
     }
-    
-    @Override
-    @SuppressWarnings("depreciation")
-    public String validade(Date fabricacao){
-            SimpleDateFormat fs = new SimpleDateFormat("dd/MM/yyyy");
-            
-            fabricacao.setDate(fabricacao.getDate() + 5);
+/* 
+public LocalDate validade(LocalDate fabricacao){
+    LocalDate dataAtual = LocalDate.now();
+    return dataAtual;
+}
 
-            return fs.format(fabricacao);
+public LocalDate validade(LocalDate fabricacao, LocalDate validade){
+    
+    LocalDate dataAtual = LocalDate.now();
+    
+    fabricacao.plusDays(1);
+    
+    return fabricacao;
+}
+
+*/    
+public static LocalDate validade(LocalDate fabricacao, LocalDate validade){
+    // Obtem a data atual
+    LocalDate dataAtual = LocalDate.now();
+    
+    // Adiciona um dia à data de fabricação (atenção: LocalDate é imutável, então o resultado precisa ser atribuído)
+    fabricacao = fabricacao.plusDays(30);
+    
+    // Retorna a data de fabricação atualizada
+    return fabricacao;
+}
+    public static void main(String[] args) {
+        // Exemplo de uso da função validade
+        LocalDate dataFabricacao = LocalDate.now(); // Data de fabricação
+        LocalDate dataValidade = LocalDate.of(2024, 7, 18); // Data de validade
+        
+        // Chamando o método validade e capturando o resultado
+        LocalDate novaDataFabricacao = validade(dataFabricacao, dataValidade);
+        
+        // Imprimindo o resultado para verificar
+        System.out.println("Data de fabricação atualizada: " + novaDataFabricacao);
+        System.out.println(validade(novaDataFabricacao, dataValidade));
     }
+    
 }
