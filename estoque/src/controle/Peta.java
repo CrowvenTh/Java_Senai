c322c59b47c596bca0cc7fa5fafbbe058519ae26
CrowvenTh <129080167+CrowvenTh@users.noreply.github.com>
2024-06-19 14:19:56 -0300

    estoque update

--- estoque/src/controle/Peta.java
+++ estoque/src/controle/Peta.java
@@ -3,35 +3,33 @@ package controle;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Peta extends Produto{
+public class Peta extends Produto {
     private String sabor;
 
     public String getSabor() {
         return sabor;
     }
+
     public void setSabor(String sabor) {
         this.sabor = sabor;
     }
-
-    @Override
-    @SuppressWarnings("depreciation")
-    public String validade(Date fabricacao){
-            SimpleDateFormat fs = new SimpleDateFormat("dd/MM/yyyy");
-            
-            fabricacao.setDate(fabricacao.getDate() + 1);
-
-            return fs.format(fabricacao);
-    }
     
-    public double Venda(int pacote){
+    @Override
+    @SuppressWarnings("deprecation")
+    public String validade(Date fabricacao){
+        SimpleDateFormat fs = new SimpleDateFormat("dd/MM/yyyy");
+  
+        fabricacao.setDate(fabricacao.getDate() + 30);
+
+        return fs.format(fabricacao);
+
+    }
+
+    public double venda( int pacote){
         return pacote * 6.70;
     }
-    public double Venda(double quilo, int id){
-        return quilo * 15.45;
-    }
-    public static void main(String[] args) {
-        Peta myPeta = new Peta();
-        System.out.println("R$ "+myPeta.Venda(34));
 
+    public double venda(double quilo){
+        return quilo * 15.45;
     }
 }
--- estoque/src/controle/Produto.java
+++ estoque/src/controle/Produto.java
@@ -1,41 +1,43 @@
 package controle;
 
-import java.util.Date;
 import java.text.SimpleDateFormat;
-
+import java.util.Date;
 
 public class Produto {
     private String nome;
-    private int qtd;
+    private int quantidade;
     private double valor;
 
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
-    public void setNome(String nome){
-         this.nome = nome;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
-    public int getQtd(){
-        return qtd;
+    public int getQuantidade() {
+        return quantidade;
     }
-    public void setQtd(int qtd){
-         this.qtd = qtd;
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
     }
-    public double getValor(){
+    public double getValor() {
         return valor;
     }
-    public void setValor(double valor){
+    public void setValor(double valor) {
         this.valor = valor;
     }
 
-    // @Override
-    @SuppressWarnings("depreciation")
+     
+    @SuppressWarnings("deprecation")
     public String validade(Date fabricacao){
-            SimpleDateFormat fs = new SimpleDateFormat("dd/MM/yyyy");
-            
-            fabricacao.setDate(fabricacao.getDate() + 5);
+        SimpleDateFormat fs = new SimpleDateFormat("dd/MM/yyyy");
+  
+        fabricacao.setDate(fabricacao.getDate() + 180);
 
-            return fs.format(fabricacao);
+        return fs.format(fabricacao);
+
     }
-        Produto pao = new Produto();
+
+    
+
 }
