3e705c8485b6df39935844d0f5b02a95781159eb
CrowvenTh <129080167+CrowvenTh@users.noreply.github.com>
2024-06-25 16:31:57 -0300

    25/6/24

---
+++ ManipulaArquivos/src/Exercicio/Main.java
@@ -0,0 +1,13 @@
+package Exercicio;
+
+import java.io.IOException;
+
+public class Main {
+public static void main(String[] args) throws Exception {
+    String way = "src\\tabuada.txt";
+
+    ManipuladorArquivoII.TextWriter(way);
+    ManipuladorArquivoII.Reader(way);
+
+}
+}
--- ManipulaArquivos/src/Manipula.java
+++ ManipulaArquivos/src/Manipula.java
@@ -1,8 +1,7 @@
 public class Manipula {
     public static void main(String[] args) throws Exception {
         String caminho = "src\\teste.txt";
-        
-        
+
         ManipuladorArquivos.Escrita(caminho);
         ManipuladorArquivos.Leitura(caminho);
     }
