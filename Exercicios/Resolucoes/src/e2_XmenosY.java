4bb43267c955ef02177cf9d534e4731226d06bcd
CrowvenTh <129080167+CrowvenTh@users.noreply.github.com>
2024-07-03 16:33:41 -0300

    add ex2

---
+++ Exercicios/Resolucoes/src/e2_XmenosY.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class e2_XmenosY {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Insira o valor de A");
+        int A = entrada.nextInt();
+        System.out.println("Insira o valor de B");
+        int B = entrada.nextInt();
+        System.out.println("O resultado é "+(A - B));
+    }
+}
