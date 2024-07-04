36cd11c2f9e8fb1c36c77e4883dbbf30df0e1079
CrowvenTh <129080167+CrowvenTh@users.noreply.github.com>
2024-07-04 14:57:27 -0300

    add auas BD

---
+++ BD_Senai/Clinica/ClinicaConsultas.sql
@@ -0,0 +1,30 @@
+use clinica;
+
+-- tabelas --
+
+select * from medicamentoPresc;
+select * from receita;
+select * from telefone;
+select * from PossuiEspecialidade;
+select * from consulta;
+select * from especialidade;
+select * from paciente;
+select * from medico;
+
+-- consultas --
+
+select m.crm as CRM, m.estadoCrm as UF, m.nomeMed as Nome, e.nomeEspecialidade as Especialidade
+	from medico as m
+		inner join especialidade as e
+			on e.idEspecialidade = m.crm;
+
+drop view medico_especialidade_cardiologia;
+
+create view medico_especialidade_cardiologia (nomeMed, crm, estadoCrm)
+	as select m.nomeMed, m.crm, m.estadoCrm from medico as m, possuiespecialidade as pe
+    where m.crm = pe.numeroCrm and pe.idEspecialidade = 3;
+    
+select * from medico_especialidade_cardiologia;
+
+select p.idPaciente, p.nomePaciente,p.cidade,p.estado, p.cep, p.sexo from paciente as p
+	where nomePaciente like '%Antonio%' group by nomePaciente asc; -- and cidade like '%Taguatinga%';
--- BD_Senai/Clinica/clinicaPopula.sql
+++ BD_Senai/Clinica/clinicaPopula.sql
@@ -1,37 +1,34 @@
 use Clinica;
 
+
 insert into medico (crm, estadoCrm, nomeMed) values
 (123456,'DF', 'Augusto Moraes Sales'),
 (654321,'PA','Miranda Casades Botella'),
 (768593,'DF','Carlos Mandrake feiosa');
-select * from medico;
 
 insert into paciente(idPaciente, sexo, cidade, estado, complemento, cep, nascimento, nomePaciente) values
 (null,'F','Abaitetuba','PA','Rua 4', 12345678,'2000-09-30','Melissa Almeida'),
 (null, 'M','Taguatinga','DF','Avenida 8', 87654321, '1998-11-28','Antonio Gomes'),
-(null, 'F','Samambaia','DF','Quadra 520', 75869483,'1995-10-12','Rafaela Duarte Lima');
-select * from paciente;
-
-show tables;
+(null, 'F','Samambaia','DF','Quadra 520', 75869483,'1995-10-12','Rafaela Duarte Lima'),
+(NULL, 'M', 'Taguatinga', 'DF', 'Rua das Flores', 98765432, '1988-05-20', 'Pedro Antonio Silva'),
+(NULL, 'F', 'Plano Piloto', 'DF', 'SHS Quadra 6', 65432109, '1992-12-15', 'Carolina Santos Antonio'),
+(NULL, 'M', 'Ceilândia', 'DF', 'Quadra 23', 12345678, '1997-08-30', 'Marcos Antonio Oliveira'),
+(NULL, 'F', 'Gama', 'DF', 'Quadra 40', 45678901, '1985-04-25', 'Juliana Antonio Pereira');
 
 insert into especialidade(nomeEspecialidade, idEspecialidade) values
 ('Ortopedia',null),
 ('Dentista',null),
 ('Cardiologia',null);
-select * from especialidade;
 
 insert into consulta(idConsulta, datahora, crm, estadoCrm, idPaciente) values
 (null, '2024-06-29 16:50:30','123456','DF',2),
 (null, '2024-07-01 14:20:45','654321','PA',3),
 (null, '2024-06-30 13:45:44','768593','DF',1); 
-select * from consulta;
 
 insert into telefone(telefone, idPaciente) values
 (61988889999, 1),
 (61909907667, 2),
 (61923345446, 3);
-select * from telefone;
-select * from PossuiEspecialidade;
 
 insert into PossuiEspecialidade (idEspecialidade, numeroCrm, estadoCrm) values
 (1, 123456, 'DF'),
@@ -47,11 +44,3 @@ insert into medicamentoPresc (medicamento, idReceita) values
 ('Nosvalgina',1),
 ('Dorflex',2),
 ('Torcilax',3);
-
-select * from medicamentoPresc;
-select * from receita;
-
-select m.crm as CRM, m.estadoCrm as UF, m.nomeMed as Nome, e.nomeEspecialidade as Especialidade
-	from medico as m
-		inner join especialidade as e
-			on e.idEspecialidade = m.crm;
