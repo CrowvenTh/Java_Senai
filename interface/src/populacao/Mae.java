834379275059d064f69643d02fb261835e93cfe7
CrowvenTh <129080167+CrowvenTh@users.noreply.github.com>
2024-07-12 15:38:34 -0300

    update organização de branch

--- interface/src/populacao/Mae.java
+++
@@ -1,7 +0,0 @@
-package populacao;
-
-public interface Mae {
-    public String corOlhos(String cor);
-    public String corPele(String cor);
-    public String tipoCabelo(String tipo);
-}
--- olaMundo/src/olaMundo/OlaMundo.java
+++
@@ -1,8 +0,0 @@
-package olaMundo;
-
-public class OlaMundo {
-
-	public static void main(String[] args) {
-		System.out.println("Olá Mundo!");
-	}
-}
