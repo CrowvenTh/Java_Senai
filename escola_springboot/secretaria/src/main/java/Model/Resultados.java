c06c21560a2babd7e54261bc5866b17397cb3ff1
CrowvenTh <129080167+CrowvenTh@users.noreply.github.com>
2024-07-23 15:31:05 -0300

    add

---
+++ escola_springboot/secretaria/src/main/java/Model/Resultados.java
@@ -0,0 +1,41 @@
+package Model;
+
+
+
+@Entity
+@Table(name = "Resultdos")
+public class Resultados {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "Id RES")
+    private long idRes;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "Matricula")
+    public Aluno getMatricula(){
+
+        @OneToOne(cascade = CascadeType.ALL)
+        @JoinColumn(name = "Id Disciplina", referencedColumnName = "Id Disciplina")
+        private Disciplinas idDis;
+        
+        public long getId() {
+            return id;
+        }
+    
+    
+        public long getId() {
+            return id;
+        }
+    
+        public void setId(long id) {
+            this.id = id;
+        }
+        public void setId(long id) {
+            this.id = id;
+        }
+
+    }
+
+}
+
+
