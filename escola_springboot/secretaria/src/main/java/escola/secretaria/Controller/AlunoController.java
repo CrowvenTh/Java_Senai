2abee36b8ee15d33f366e6ec06df849064b8a654
CrowvenTh <129080167+CrowvenTh@users.noreply.github.com>
2024-07-23 16:51:23 -0300

    Update AlunoController.java

--- escola_springboot/secretaria/src/main/java/escola/secretaria/Controller/AlunoController.java
+++ escola_springboot/secretaria/src/main/java/escola/secretaria/Controller/AlunoController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import Model.Aluno;
 import escola.secretaria.repository.AlunoRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class AlunoController {
@@ -18,4 +21,10 @@ public class AlunoController {
     public List<Aluno> lista() {
         return alunoRepository.findAll();
     }
+    @PostMapping("/inserir")
+    public Aluno inserirAluno(@RequestBody String Aluno aluno) {
+        return alunoRepository.save(aluno);
+
+    }
+    
 }
