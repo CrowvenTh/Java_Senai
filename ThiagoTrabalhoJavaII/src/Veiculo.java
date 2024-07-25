6326d38f8d332c44c9ed822cb4f60d510ac6f929
CrowvenTh <129080167+CrowvenTh@users.noreply.github.com>
2024-07-25 15:19:14 -0300

    Classe Veiculo
    essa classe conterá todos os devidos dados relacionados especificamente aos carros da empresa

---
+++ ThiagoTrabalhoJavaII/src/Veiculo.java
@@ -0,0 +1,111 @@
+public class Veiculo {
+    private String marca;
+    private String modelo;
+    private String anoModelo;
+    private String anoFabricacao;
+    private double motorizacao;
+    private double capacidadeTanque;
+    private String tipoCombustivel;
+    private String cor;
+    private String placa;
+    private int renavam;
+    private int numeroPneus;
+    private int numeroPassageiros;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getAnoModelo() {
+        return anoModelo;
+    }
+
+    public void setAnoModelo(String anoModelo) {
+        this.anoModelo = anoModelo;
+    }
+
+    public String getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(String anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public double getMotorizacao() {
+        return motorizacao;
+    }
+
+    public void setMotorizacao(double motorizacao) {
+        this.motorizacao = motorizacao;
+    }
+
+    public double getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    public void setCapacidadeTanque(double capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    public String getTipoCombustivel() {
+        return tipoCombustivel;
+    }
+
+    public void setTipoCombustivel(String tipoCombustivel) {
+        this.tipoCombustivel = tipoCombustivel;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getRenavam() {
+        return renavam;
+    }
+
+    public void setRenavam(int renavam) {
+        this.renavam = renavam;
+    }
+
+    public int getNumeroPneus() {
+        return numeroPneus;
+    }
+
+    public void setNumeroPneus(int numeroPneus) {
+        this.numeroPneus = numeroPneus;
+    }
+
+    public int getNumeroPassageiros() {
+        return numeroPassageiros;
+    }
+
+    public void setNumeroPassageiros(int numeroPassageiros) {
+        this.numeroPassageiros = numeroPassageiros;
+    }
+
+}
