a4e7641c4ec7d8c399f46865e266a3f38dc96fde
CrowvenTh <129080167+CrowvenTh@users.noreply.github.com>
2024-07-25 17:40:17 -0300

    tentativa de listar veiculos

---
+++ ThiagoTrabalhoJavaII/src/manutencao.java
@@ -0,0 +1,12 @@
+public class manutencao extends Gastos{
+    private String tipo;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+}
