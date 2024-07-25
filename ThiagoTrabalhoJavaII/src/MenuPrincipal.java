25a500fb2b60aa6c4b13b0928bc50673b081b2cc
CrowvenTh <129080167+CrowvenTh@users.noreply.github.com>
2024-07-25 17:38:51 -0300

    bug de ordem do menu
    o menu está em conflito e não abre os menus na forma desejada

--- ThiagoTrabalhoJavaII/src/MenuPrincipal.java
+++ ThiagoTrabalhoJavaII/src/MenuPrincipal.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 
 public class MenuPrincipal {
     public void exibirMenu() {
-        Veiculo veiculo = new Veiculo();
+
         while (true) {
             String[] options = new String[] { "Cadastrar Veículo", "Veículos Cadastrados", "Gestão de Gastos", "Sair" };
             int escolha = JOptionPane.showOptionDialog(
@@ -13,21 +13,51 @@ public class MenuPrincipal {
                     JOptionPane.PLAIN_MESSAGE,
                     null,
                     options,
+                    options[1]);
+
+            switch (escolha) {
+                case 1:
+                    Veiculo veiculo = new Veiculo();
+                    veiculo.cadastrarVeiculo();
+                    break;
+                case 2:
+                    // veiculo.veiculosCadastrados();
+                    break;
+                case 3:
+                    // Lógica para "Gestão de Gastos"
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+            }
+        }
+    }
+
+    public void menuGastos() {
+        Gastos gasto = new Gastos();
+        while (true) {
+            String[] options = new String[] { "Cadastrar Despesa", "Mostrar Despesas Cadastradas", "Voltar" };
+            int escolha = JOptionPane.showOptionDialog(
+                    null,
+                    "Selecione",
+                    "Gestão de Gastos",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    options,
                     options[0]);
 
             switch (escolha) {
                 case 0:
-                    veiculo.cadastrarVeiculo();
+                    gasto.cadastrarGasto();
                     break;
                 case 1:
-                    veiculo.veiculosCadastrados();
+                    gasto.mostrarGasto();
                     break;
                 case 2:
-                    // Lógica para "Gestão de Gastos"
-                    break;
-                case 3:
                     System.exit(0);
-                    break;
+                default:
+                    throw new AssertionError();
             }
         }
     }
