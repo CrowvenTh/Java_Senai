64033640a34e78040359c0b6cdb18b9bc3d885f0
CrowvenTh <129080167+CrowvenTh@users.noreply.github.com>
2024-08-12 17:14:10 -0300

    limpeza de branch e preparação de ambiente

--- interface/src/FormasGeometricas/Formas.java
+++
@@ -1,7 +0,0 @@
-package FormasGeometricas;
-
-public interface Formas {
-    public String getNomeFigura();
-    public double getArea();
-    public double getPerimetro();
-}
--- interface/src/automovel/Proprietario.java
+++
@@ -1,7 +0,0 @@
-package automovel;
-
-public interface Proprietario {
-    public String getNome(String nome);
-    public String getEndereco();
-
-}
