cfb6ef9a5cf0e8c544b7d9d4855419b9bdc1bf67
CrowvenTh <129080167+CrowvenTh@users.noreply.github.com>
2024-08-20 17:47:13 -0300

    Update UsuarioRepository.java

--- verificacao/src/main/java/com/autenticacao/Repository/UsuarioRepository.java
+++ verificacao/src/main/java/com/autenticacao/Repository/UsuarioRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.autenticacao.Model.RegraModel;
 import com.autenticacao.Model.UsuarioModel;
 
-public interface UsuarioRepository extends JpaRepository<RegraModel, Long>{
+public interface UsuarioRepository extends JpaRepository<RegraModel, Long> {
 
     Optional<UsuarioModel> findByEmail(String email);
 
